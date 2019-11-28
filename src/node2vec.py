b9c006fee56e3ccea442e953a6365bc1edae9894
zxk19981227 <40291547+zxk19981227@users.noreply.github.com>
2019-11-28 17:48:04 +0800

    Update node2vec.py

--- src/node2vec.py
+++ src/node2vec.py
@@ -13,20 +13,21 @@ class Graph():
 	def node2vec_walk(self, walk_length, start_node):
 		'''
 		Simulate a random walk starting from start node.
+		
 		'''
 		G = self.G
-		alias_nodes = self.alias_nodes
+		alias_nodes = self.alias_nodes#这是进行采样时候使用的点和边，但是还没有进行set成为O(1)的函数
 		alias_edges = self.alias_edges
 
-		walk = [start_node]
+		walk = [start_node]#start node不是全体的点，仅仅是一个开始
 
 		while len(walk) < walk_length:
-			cur = walk[-1]
+			cur = walk[-1]#直接从最后一次走的点进行处理
 			cur_nbrs = sorted(G.neighbors(cur))
 			if len(cur_nbrs) > 0:
-				if len(walk) == 1:
+				if len(walk) == 1:#判断walk行走的步数，我有点疑惑为什么这里需要对于walk进行处理
 					walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
-				else:
+				else:#看了好久终于明白了这里传递prev是为了确定上一个节点从而方便计算概率
 					prev = walk[-2]
 					next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], 
 						alias_edges[(prev, cur)][1])]
@@ -42,7 +43,7 @@ class Graph():
 		'''
 		G = self.G
 		walks = []
-		nodes = list(G.nodes())
+		nodes = list(G.nodes())#这里转化格式
 		print 'Walk iteration:'
 		for walk_iter in range(num_walks):
 			print str(walk_iter+1), '/', str(num_walks)
@@ -60,7 +61,7 @@ class Graph():
 		p = self.p
 		q = self.q
 
-		unnormalized_probs = []
+		unnormalized_probs = []#这里是一个概率化过程，对于每一个点直接查找下一个点的类型从而从三个bias中获得正确的加成
 		for dst_nbr in sorted(G.neighbors(dst)):
 			if dst_nbr == src:
 				unnormalized_probs.append(G[dst][dst_nbr]['weight']/p)
@@ -68,7 +69,7 @@ class Graph():
 				unnormalized_probs.append(G[dst][dst_nbr]['weight'])
 			else:
 				unnormalized_probs.append(G[dst][dst_nbr]['weight']/q)
-		norm_const = sum(unnormalized_probs)
+		norm_const = sum(unnormalized_probs)#对于节点的维度构建alias数组从而达到牺牲空间节省时间的效果
 		normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
 
 		return alias_setup(normalized_probs)
@@ -83,21 +84,22 @@ class Graph():
 		alias_nodes = {}
 		for node in G.nodes():
 			unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
-			norm_const = sum(unnormalized_probs)
-			normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
-			alias_nodes[node] = alias_setup(normalized_probs)
+			norm_const = sum(unnormalized_probs)#对邻居节点所有的边进行求和
+			normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]#概率
+			alias_nodes[node] = alias_setup(normalized_probs)#这里对于每一个节点都建立了这样一个alias的概率表
 
 		alias_edges = {}
 		triads = {}
 
 		if is_directed:
 			for edge in G.edges():
-				alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
+				alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])#我看迷了这边，然后才反应过来edge中0、1分别代表两个端点
 		else:
 			for edge in G.edges():
 				alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-				alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
-
+				alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])#在增加一个从edge1-》0的aliaslist，我觉得差异不是很大但是还是存在
+				
+		#这样就可以结束了，所有的alias点和边都存储了alias表但是并不可以直接搜索，这里只是一个概率，还没通过set-up进行创建N的算法
 		self.alias_nodes = alias_nodes
 		self.alias_edges = alias_edges
 
@@ -142,7 +144,7 @@ def alias_draw(J, q):
 	'''
 	K = len(J)
 
-	kk = int(np.floor(np.random.rand()*K))
+	kk = int(np.floor(np.random.rand()*K))#这里直接生成了一个随机数,K代表的是这个点由多少个邻接节点
 	if np.random.rand() < q[kk]:
 	    return kk
 	else:
